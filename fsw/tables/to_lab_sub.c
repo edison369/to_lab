eeeddc116b9fd94e29df4eeebdabe42011d02940
Edison Martinez <e.dimar1999@gmail.com>
2022-10-27 21:27:21 -0500

    Added Blinky app to sub table

--- fsw/tables/to_lab_sub.c
+++ fsw/tables/to_lab_sub.c
@@ -37,6 +37,7 @@
 #include "ci_lab_msgids.h"
 
 #include "sample_app_msgids.h"
+#include "blinky_msgids.h"
 
 #if 0
 #include "hs_msgids.h"
@@ -51,6 +52,7 @@ TO_LAB_Subs_t TO_LAB_Subs = {.Subs = {/* CFS App Subscriptions */
                                       {CFE_SB_MSGID_WRAP_VALUE(TO_LAB_DATA_TYPES_MID), {0, 0}, 4},
                                       {CFE_SB_MSGID_WRAP_VALUE(CI_LAB_HK_TLM_MID), {0, 0}, 4},
                                       {CFE_SB_MSGID_WRAP_VALUE(SAMPLE_APP_HK_TLM_MID), {0, 0}, 4},
+                                      {CFE_SB_MSGID_WRAP_VALUE(BLINKY_HK_TLM_MID), {0, 0}, 4},
 
 #if 0
         /* Add these if needed */
